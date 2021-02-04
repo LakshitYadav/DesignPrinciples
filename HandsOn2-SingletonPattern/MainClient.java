c42a84668a473b57d6f85786db8391ce1aba0802
Lakshit Yadav <59692570+LakshitYadav@users.noreply.github.com>
2021-02-04 17:07:24 +0530

    Delete HandsOn2-SingletonPattern directory

--- HandsOn2-SingletonPattern/MainClient.java
+++
@@ -1,8 +0,0 @@
-public class MainClient {
-	public static void main(String[] args) {
-		DBConn obj1=DBConn.getInstance();
-		System.out.println(obj1.hashCode());
-		DBConn obj2=DBConn.getInstance();
-		System.out.println(obj2.hashCode());
-	}
-}
