f06a9c2818d85ff91dd800f025f35c245624d749
Lakshit Yadav <59692570+LakshitYadav@users.noreply.github.com>
2021-02-04 17:07:06 +0530

    Delete HandsOn1-SolidPrinciples/src/com/cts directory

--- HandsOn1-SolidPrinciples/src/com/cts/Order.java
+++
@@ -1,5 +0,0 @@
-package com.cts;
-
-public interface Order {
-	public void processOrder(String modelName);
-}
--- HandsOn1-SolidPrinciples/src/com/cts/Repair.java
+++
@@ -1,5 +0,0 @@
-package com.cts;
-public interface Repair {
-	public void processPhoneRepair(String modelName);
-	public void processAccessoryRepair(String accessoryType);
-}
